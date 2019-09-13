4ddef785bfab3df89c5f9cc954ead873a122e2c4
Tim Eggert <tim@elbart.com>
2019-09-13 23:16:00 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sustainerds.api",
+    version="0.0.1",
+    author="Tim Eggert",
+    author_email="tim@elbart.com",
+    description="Sustainerds API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/elbart/sustainerds",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
